eed413b5dbb852a75bb14e77cd6da471e9850534
YeonJinP <109507463+YeonJinP@users.noreply.github.com>
2023-03-16 13:22:41 +0900

    03-16

---
+++ 03-16/2480.py
@@ -0,0 +1,14 @@
+a, b, c = map(int, input().split())
+
+if a == b :
+    if b == c :
+        print(10000+a*1000)
+    elif b != c :
+        print(1000+a*100)
+elif b == c and a != b :
+    print(1000+b*100)
+elif a == c and a != b :
+    print(1000+a*100)
+else :
+    print(max(a,b,c)*100)
+ 
